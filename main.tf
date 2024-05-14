4208583c3e006f425803f30eabdf53da659620b6
PortalsAllTheWayDown <167829455+PortalsAllTheWayDown@users.noreply.github.com>
2024-05-14 00:58:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,4 +21,8 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  client_id       = "${{secrets.AZURE_CLIENT_ID}}"
+  client_secret   = "${{secrets.AZURE_CLIENT_SECRET}}"
+  tenant_id       = "${{var.AZURE_TENANT_ID}}"
+  subscription_id = "${{var.AZURE_SUBSCRIPTION_ID}}"
 }
