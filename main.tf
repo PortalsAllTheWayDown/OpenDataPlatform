2aca51d289b8f8b207d39f791aeb05945381362d
PortalsAllTheWayDown <167829455+PortalsAllTheWayDown@users.noreply.github.com>
2024-05-14 12:28:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
 terraform {
+  required_version = ">= 1.5.7"
   backend "azurerm" {
     resource_group_name   = "state"
     storage_account_name  = "stateforterraform"
     container_name        = "state"
     key                   = "terraform.tfstate"
-  }
-}
-terraform {
-  required_version = ">= 1.5.7"
-  backend "azurerm" {
-    resource_group_name  = "thomasthorntoncloud"
-    storage_account_name = "thomasthorntontfstate"
-    container_name       = "github-thomasthorntoncloud-terraform-example"
-    key                  = "github-thomasthorntoncloud-terraform-example.tfstate"
   }
 }
  
@@ -23,7 +15,7 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
  
 #Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "github-thomasthorntoncloud-terraform-example"
-  location = "uksouth"
+resource "azurerm_resource_group" "test" {
+  name     = "testing"
+  location = "us east"
 }
