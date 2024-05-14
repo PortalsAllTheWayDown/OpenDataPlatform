f0654368880e38577fdca9bf9049608d2f7cb32d
PortalsAllTheWayDown <167829455+PortalsAllTheWayDown@users.noreply.github.com>
2024-05-14 00:43:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "state"
+    storage_account_name  = "stateforterraform"
+    container_name        = "state"
+    key                   = "terraform.tfstate"
+  }
+}
 resource "azurerm_resource_group" "opendataplatform" {
   name     = "OpenDataPlatform"
   location = "East US"
